6e28b4ae4ee1ac126e50ea97aefcba5ee373fba1
Vlad Tomici <vladtomici@gmail.com>
2025-06-03 17:25:40 +0300

    fixed the importing issues

--- src/util.c
+++ src/util.c
@@ -7,3 +7,10 @@ Vector2f createVector(float x, float y) {
     
     return temp;
 }
+
+const int squareSize = 100;
+const int sidebar1_width = 300;
+const int sidebar2_width = 300;
+const int boardWidth = squareSize * 8;
+const int screenWidth = boardWidth + sidebar1_width + sidebar2_width;
+const int screenHeight = squareSize * 8;
